fd0f8de3801bc5b66a4570e9ea263e3e1ffe5f38
William Matkaselkä <william.matkaselka@hotmail.com>
2025-01-04 16:25:02 +0100

    debugging frontend build

--- src/utils/docker.py
+++ src/utils/docker.py
@@ -5,19 +5,24 @@ class Docker:
     class Compose:
         @staticmethod
         def build(directory, npm=False, reinstall=False):
-            if npm:
-                print("Running npm install and npm run build...")
+            if npm and (reinstall or not os.path.exists(os.path.join(directory, 'build'))):
                 try:
-                    if reinstall or not os.path.join(directory, 'build'):
-                        Command.run(["npm", "ci"], directory=directory)
                     Command.run(["npm", "ci"], directory=directory)
-                    Command.run(["npm", "run", "build"], directory=directory)
-                    print("NPM build completed successfully.")
+                    print("Running npm install...")
                 except Exception as e:
-                    print(f"Failed to run NPM commands: {e}")
+                    print(f"Failed to run NPM install: {e}")
+                    print(f"/build exists: {os.path.exists(os.path.join(directory, 'build'))}")
+                    return
+            if npm:
+                try:
+                    print("Running npm run build...")
+                    Command.run(["npm", "run", "build"], directory=directory)
+                except Exception as e:
+                    print(f"Failed to run NPM build: {e}")
                     return
             try:
                 print("Building the Docker image...")
+                
                 Command.run(["docker-compose", "build"], directory=directory)
                 print("Docker image built successfully.")    
             except Exception as e:
