327cee69f2e6050936c6919c3d2c20cb5162924a
William Matkaselkä <william.matkaselka@hotmail.com>
2025-01-06 23:38:56 +0100

    added docker setup for simulation folder (working)

--- simulation/src/main.py
+++ simulation/src/main.py
@@ -5,7 +5,15 @@ if __name__ == "__main__":
 
     get = Get()
     bikes = get.bikes(save_to_json=False, fallback=False)
-    print(bikes)
+    users = get.users(save_to_json=False, fallback=False)
+    trips = get.trips(save_to_json=False, fallback=False)
+    zones = get.zones(save_to_json=False, fallback=False)
+    zone_types = get.zone_types(save_to_json=False, fallback=False)
+    print(users)
 
 
     # see Makefile in root for instructions for how to run the simulation 
+
+    # 1. Start the simulation.
+    # 2. Get and extract users, trips, and bikes data.
+    # ???
