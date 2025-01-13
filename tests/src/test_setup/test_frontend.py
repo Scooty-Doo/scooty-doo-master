97cd7a097857082f7898160bcb0a5b77cb8f157d
William Matkaselkä <william.matkaselka@hotmail.com>
2025-01-13 22:13:06 +0100

    remove more irrelevant tests

--- tests/src/test_setup/test_frontend.py
+++ tests/src/test_setup/test_frontend.py
@@ -1,11 +1,6 @@
 from unittest.mock import patch
 from src.setup._frontend import Frontend
 
-@patch("src.setup._frontend.Frontend.Docker._build")
-def test_frontend_setup(mock_build):
-    Frontend.setup()
-    mock_build.assert_called_once()
-
 @patch("src.setup._frontend.Frontend.Docker._start")
 @patch("src.setup._frontend.Frontend.Docker.status")
 @patch("src.setup._frontend.Frontend.Docker.logs")
